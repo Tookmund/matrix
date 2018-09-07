f065d0c52cddcaa050324b3a911c09c662803829
Jacob Adams <tookmund@gmail.com>
2018-09-07 14:30:10 -0400

    scale the matrix

--- scale.c
+++ scale.c
@@ -1,7 +1,17 @@
 #include "matrix.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
+	matrix_t *m = loadmatrix();
+	if (argc < 3)
+	{
+		puts("scale [row] [factor]");
+		return 1;
+	}
+	int row = atoi(argv[1])-1;
+	int factor = atoi(argv[2]);
+	for (int i = 0; i < m->c; i++) m->m[row][i] = m->m[row][i]*factor;
+	savematrix(m);
 	return 0;
 }
 
