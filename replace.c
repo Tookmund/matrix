f251292f40d9b94ef662ebc988c1c671b84bb6e5
Jacob Adams <tookmund@gmail.com>
2018-09-07 14:36:45 -0400

    replace matrix

--- replace.c
+++ replace.c
@@ -1,7 +1,21 @@
 #include "matrix.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
+	matrix_t *m = loadmatrix();
+	if (argc < 4)
+	{
+		puts("replace [row] [factor] [row to add]");
+		return 1;
+	}
+	int row = atoi(argv[1])-1;
+	int factor = atoi(argv[2]);
+	int rowadd = atoi(argv[3])-1;
+	for (int i = 0; i < m->c; i++)
+	{
+		m->m[row][i] = m->m[row][i]+(factor*m->m[rowadd][i]);
+	}
+	savematrix(m);
 	return 0;
 }
 
