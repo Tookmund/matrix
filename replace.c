997762b87bb05f5fa508c852cf8064356e263335
Jacob Adams <tookmund@gmail.com>
2018-09-09 14:11:46 -0400

    replace, scale: Add /factor for division

--- replace.c
+++ replace.c
@@ -4,17 +4,26 @@ int main(int argc, char **argv)
 {
 	if (argc < 4)
 	{
-		fputs("replace [row] [factor] [row to add]", stderr);
+		fputs("replace [row] [factor] [row to add] (/factor to divide)", stderr);
 		return 1;
 	}
 	matrix_t *m = loadmatrix();
 	if (m == NULL) return 2;
+	char div = 0;
 	int row = atoi(argv[1])-1;
+	if (argv[2][0] == '/')
+	{
+		div = 1;
+		argv[2]++;
+	}
 	int factor = atoi(argv[2]);
 	int rowadd = atoi(argv[3])-1;
 	for (int i = 0; i < m->c; i++)
 	{
-		m->m[row][i] = m->m[row][i]+(factor*m->m[rowadd][i]);
+		int add = m->m[rowadd][i];
+		if (div) add = add/factor;
+		else add = add*factor;
+		m->m[row][i] = m->m[row][i]+add;
 	}
 	savematrix(m);
 	return 0;
--- scale.c
+++ scale.c
@@ -4,14 +4,26 @@ int main(int argc, char **argv)
 {
 	if (argc < 3)
 	{
-		fputs("scale [row] [factor]", stderr);
+		fputs("scale [row] [factor] (/factor to divide)", stderr);
 		return 1;
 	}
 	matrix_t *m = loadmatrix();
 	if (m == NULL) return 2;
 	int row = atoi(argv[1])-1;
+	char div = 0;
+	if (argv[2][0] == '/')
+	{
+		div = 1;
+		argv[2]++;
+	}
 	int factor = atoi(argv[2]);
-	for (int i = 0; i < m->c; i++) m->m[row][i] = m->m[row][i]*factor;
+	for (int i = 0; i < m->c; i++)
+	{
+		int add = m->m[row][i];
+		if (div) add = add/factor;
+		else add = add*factor;
+		m->m[row][i] = add;
+	}
 	savematrix(m);
 	return 0;
 }
